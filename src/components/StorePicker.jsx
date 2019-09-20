7356004903cde734d3b9cc0238f20958fe468dc6
Tony Tea <tonybountea@gmail.com>
2019-09-19 20:30:01 -0600

    Add button click event

--- src/components/StorePicker.jsx
+++ src/components/StorePicker.jsx
@@ -3,32 +3,17 @@ import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
   
-  // set property instead of a constructor
-  // constructor(){
-  //   super();
-  //   this.goToStore = this.goToStore.bind(this); // reference this as the go to store as the storepicker
-  // }
-
   myInput = React.createRef();
 
-  // for render can only return one element so move elements into parent element
-  // could be an issue if returning multiple flexbox
-  // wrap them in a react.fragment tag, allows you to return as many adjacent elements as you want
-  // Using react fragmnent will clean up the html, wont have unnecessary divs
-  // could either import Fragment with react or import as React.Fragment
   goToStore = event => {
     event.preventDefault(); 
-    console.log(this);
+    const storeName = this.myInput.current.value;
+    this.props.history.push(`/store/${storeName}`);
   };
 
-
-  // all the built in methods from react are from the mom component the react componenets
-  // other methods are not bound by default so cant simply use this on your own methods
-  // so we must bind our own methods
   render() {
     return (
       <form className="store-selector" onSubmit={this.goToStore}>
-        {/* comment, jsx does not use // must use curly brace */}
         <h2>Please Enter a Store</h2>
         <input
           type="text"
@@ -44,4 +29,3 @@ class StorePicker extends React.Component {
 }
 
 export default StorePicker;
-// made some changes

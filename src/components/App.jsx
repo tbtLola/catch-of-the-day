f2e19677a9471d44f4983b31b7a93e3446b753d9
Tony Tea <tonybountea@gmail.com>
2019-09-22 22:20:10 -0600

    persisting our state using firebase

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,66 +1,82 @@
-import React from 'react';
-import Header from './Header'
-import Inventory from './Inventory'
-import Order from './Order'
-import sampleFishes from '../sample-fishes'
-import Fish from "./Fish"
+import React from "react";
+import Header from "./Header";
+import Inventory from "./Inventory";
+import Order from "./Order";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
+import base from "../base";
 
 // methods that update state must live in the same component as where the state lives
 class App extends React.Component {
-    state = {
-        fishes:{},
-        order: {}
-    };
+  state = {
+    fishes: {},
+    order: {}
+  };
 
-    loadSampleFishes = () => {
-        this.setState({fishes:sampleFishes});   
-    };
+  componentDidMount() {
+    const { params } = this.props.match;
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+      context: this,
+      state: "fishes"
+    });
+  }
 
-    addToOrder = (key) => {
-        // 1. Take a copy of state
-        const order ={...this.state.order};
-        // 2. Either add to order or update the number in the order
-        order[key] = order[key] + 1 || 1; 
-        // 3. Call set state to update our state object
-        this.setState({order});
-    };
+  // use this to prevent memoryleak , must clean up afterwards
+  
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+}
 
-    addFish = (fish) => {
-        // Updating state
-        // 1. Take a copy of existing state (you dont want to modify the actual state)
-        const fishes = {...this.state.fishes} // takes a copy of everything
-        // 2. Add our new fish to that fishes variable
-        fishes[`fish${Date.now()}`] = fish; // randomly generating a unique key
-        // 3. Set the new fishes object to state
-        // only need to update the pieces that we only need
-        this.setState({
-            fishes
-        });
-    };
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
 
-    render(){
-        return (
-            <div className="catch-of-the-day">
-                <div className="menu">
-                    <Header tagline="Fresh Seafood market"/>
-                <ul className="fishes">
-                    {Object.keys(this.state.fishes).map(key => (
-                        <Fish
-                         key={key}
-                         details={this.state.fishes[key]} 
-                         addToOrder={this.addToOrder} 
-                         index={key}/> 
-                    ))}
-                </ul>
-                </div>
-                <Order fishes={this.state.fishes} order={this.state.order}/>
-                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
-            </div>
+  addToOrder = key => {
+    // 1. Take a copy of state
+    const order = { ...this.state.order };
+    // 2. Either add to order or update the number in the order
+    order[key] = order[key] + 1 || 1;
+    // 3. Call set state to update our state object
+    this.setState({ order });
+  };
 
-        )
-    }
-} 
+  addFish = fish => {
+    // Updating state
+    // 1. Take a copy of existing state (you dont want to modify the actual state)
+    const fishes = { ...this.state.fishes }; // takes a copy of everything
+    // 2. Add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish; // randomly generating a unique key
+    // 3. Set the new fishes object to state
+    // only need to update the pieces that we only need
+    this.setState({
+      fishes
+    });
+  };
 
-export default App; 
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+                index={key}
+              />
+            ))}
+          </ul>
+        </div>
+        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
+      </div>
+    );
+  }
+}
 
- 
+export default App;
--- src/components/StorePicker.jsx
+++ src/components/StorePicker.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
